b5be59f7f3b9b9fcf7d946868f27369db732ea69
xausky <xausky@gmail.com>
2016-09-30 15:13:20 +0800

    [FIX]init.sh

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,6 @@ MAINTAINER xausky xausky@gmail.com
 
 ENV ALLUXIO_VERSION 1.2.0
 
-ADD http://alluxio.org/downloads/files/1.2.0/alluxio-1.2.0-bin.tar.gz /usr/local
-
 COPY init.sh /usr/local/bin/
 
 COPY entrypoint.sh /usr/local/bin/
--- init.sh
+++ init.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+curl -fSL http://alluxio.org/downloads/files/${ALLUXIO_VERSION}/alluxio-${ALLUXIO_VERSION}-bin.tar.gz -o alluxio.tar.gz
+
+tar -xzC /usr/local -f alluxio.tar.gz
+
+rm -rf alluxio.tar.gz
+
 cd /usr/local/alluxio-${ALLUXIO_VERSION}
 
 sed  -i 's/sudo//g' bin/alluxio-mount.sh
